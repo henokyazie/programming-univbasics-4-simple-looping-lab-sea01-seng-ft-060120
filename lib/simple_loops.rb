31d9f977272e3c2e2d32ec0a391dc2931ccd50ef
brave-ping-4201 <h_yazie@yahoo.com>
2020-04-29 06:54:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,7 @@
 # Write your methods here
-def loop_message_five_times(string)
-  counter = 5
+def loop_message_five_times
+  message = 
   while string[counter] do
-    puts "Hello World"
-    counter <=5
+  
   end
 end
